d81dffdedc6061df3c8a63b20fb38147a868def7
oswaldo <vnkvp@outlook.com>
2020-09-02 18:36:11 -0300

    mapping home page

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../assets/Home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1>home</h1>
+            <div className='homeHeader'>
+                <div className='homeHeaderLeft'>
+                    {/* Link */}
+                    {/* Link */}
+                </div>
+                <div className='homeHeaderRight'>
+                    {/* Link */}
+                    {/* Link */}
+                    {/* Icon */}
+                    {/* Avatar */}
+                </div>
+            </div>
+            <div className='homeBody'></div>
+        </div>
+    )
+}
+
+export default Home
