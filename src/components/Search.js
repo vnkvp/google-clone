0146ac1e77d5cbf62383a1a0938129f1389e3ea2
oswaldo <vnkvp@outlook.com>
2020-09-08 22:15:48 -0300

    homepage ui set, search stateful component mapped

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search'
 import MicIcon from '@material-ui/icons/Mic'
+import { Button } from '@material-ui/core'
 import '../assets/Search.css'
+import { useHistory } from 'react-router-dom'
 
-function Search() {
+function Search({ hideButtons = false }) {
+
+    const [input, setInput] = useState('');
+    const history = useHistory();
+
+
+    const search = e => {
+        e.preventDefault();
+        console.log('search hit');
+        history.push('/search');
+    }
+
     return (
-        <div className='search'>
+        <form className='search'>
             <div className='searchInput'>
                 <SearchIcon className='searchInputIcon' />
-                <input type='text' />
+                <input value={input} onChange={e => setInput(e.target.value)} />
                 <MicIcon />
             </div>
-        </div>
+            {!hideButtons ? (<div className='searchButtons'>
+                <Button variant='outlined' onClick={search} type='submit'>Google Search</Button>
+                <Button variant='outlined'>I'm feeling lucky</Button>
+            </div>) : (<div className='searchButtons'>
+                <Button variant='outlined' onClick={search} type='submit' className='buttonsHidden'>Google Search</Button>
+                <Button variant='outlined' className='buttonsHidden'>I'm feeling lucky</Button>
+            </div>)}
+        </form>
     )
 }
 
